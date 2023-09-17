3f7dbdaf5a72991f2bc339764b51a908fac71077
stefano <stefano0012@gmail.com>
2023-09-17 09:30:11 +0200

    add cart and shop pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:cofeeshop/components/bottom_nav_bar.dart';
 import 'package:cofeeshop/const.dart';
+import 'package:cofeeshop/pages/cart_page.dart';
+import 'package:cofeeshop/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,11 +12,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // navigate bottom bar
+  int _selectedIndex = 0;
+  void navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    // shop page
+    ShopPage(),
+
+    // cart page
+    CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      bottomNavigationBar: MyBottomNavBar(),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomNavBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
